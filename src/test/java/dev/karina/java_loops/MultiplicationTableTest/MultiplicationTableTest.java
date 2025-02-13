7da7e1fabd12fa60bac48f01b67825c9bcdb4aec
karinareyes25 <karinalrg16@gmail.com>
2025-02-13 17:37:02 +0100

    testMultiplicationTableNegative implemented. Test passed

--- src/test/java/dev/karina/java_loops/MultiplicationTableTest/MultiplicationTableTest.java
+++ src/test/java/dev/karina/java_loops/MultiplicationTableTest/MultiplicationTableTest.java
@@ -32,4 +32,19 @@ public class MultiplicationTableTest{
                 "5 x 7 = 35", "5 x 8 = 40", "5 x 9 = 45",
                 "5 x 10 = 50"));
     }
+
+    @Test
+    @DisplayName("Test the multiplication table of -5")
+    @Tag("Multiplication")
+    public void testMultiplicationTableNegative() {
+        int n = -5;
+        List<String> expected = List.of(
+            "-5 x 1 = -5", "-5 x 2 = -10", "-5 x 3 = -15",
+            "-5 x 4 = -20", "-5 x 5 = -25", "-5 x 6 = -30",
+            "-5 x 7 = -35", "-5 x 8 = -40", "-5 x 9 = -45",
+            "-5 x 10 = -50"
+        );
+        List<String> result = MultiplicationTable.generateTable(n);
+        assertEquals(expected, result);
+    }
 }
