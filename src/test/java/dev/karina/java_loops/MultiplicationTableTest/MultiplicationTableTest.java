3a1b6f9f9f8a7c6ad439ba1573ce2765f14c31f3
karinareyes25 <karinalrg16@gmail.com>
2025-02-13 17:20:44 +0100

    fix testMultiplicationTable. The test fails

--- src/test/java/dev/karina/java_loops/MultiplicationTableTest/MultiplicationTableTest.java
+++ src/test/java/dev/karina/java_loops/MultiplicationTableTest/MultiplicationTableTest.java
@@ -9,28 +9,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import dev.karina.java_loops.MultiplicationTable.MultiplicationTable;
+
 public class MultiplicationTableTest {
 
     @Test
     @DisplayName("Test the multiplication table of 5")
     @Tag("Multiplication")
-        public void testMultiplicationTable(){
-            int n = 5;
-            List <String> expected = List.of(
+    public void testMultiplicationTable() {
+        int n = 5;
+        List<String> expected = List.of(
                 "5 x 1 = 5", "5 x 2 = 10", "5 x 3 = 15",
                 "5 x 4 = 20", "5 x 5 = 25", "5 x 6 = 30",
                 "5 x 7 = 35", "5 x 8 = 40", "5 x 9 = 45",
-                "5 x 10 = 50"
-            );
-            List <String> result = MultiplicationTable.generateTable(n);
+                "5 x 10 = 50");
+        List<String> result = MultiplicationTable.generateTable(n);
 
-            assertThat(result, contains(
-            "5 x 1 = 5", "5 x 2 = 10", "5 x 3 = 15",
-            "5 x 4 = 20", "5 x 5 = 25", "5 x 6 = 30",
-            "5 x 7 = 35", "5 x 8 = 40", "5 x 9 = 45",
-            "5 x 10 = 50"
-        ));
-        }
+        assertThat(result, contains(
+                "5 x 1 = 5", "5 x 2 = 10", "5 x 3 = 15",
+                "5 x 4 = 20", "5 x 5 = 25", "5 x 6 = 30",
+                "5 x 7 = 35", "5 x 8 = 40", "5 x 9 = 45",
+                "5 x 10 = 50"));
     }
-    
-
+}
