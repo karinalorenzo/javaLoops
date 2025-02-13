03e5ed974548f6fcc5896d13aba88a9d6b4c1bd6
karinareyes25 <karinalrg16@gmail.com>
2025-02-13 17:28:12 +0100

    class MultiplicationTable implemented. The test run correctly

---
+++ src/main/java/dev/karina/java_loops/MultiplicationTable/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package dev.karina.java_loops.MultiplicationTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplicationTable {
+    public static List<String> generateTable(int n){
+        List<String> table = new ArrayList<>();
+            for (int i = 1; i <= 10; i++){
+                table.add(n + " x " + i + " = " + (n * i));
+            }
+            return table;
+    }
+}
--- src/test/java/dev/karina/java_loops/MultiplicationTableTest/MultiplicationTableTest.java
+++ src/test/java/dev/karina/java_loops/MultiplicationTableTest/MultiplicationTableTest.java
@@ -2,16 +2,16 @@ package dev.karina.java_loops.MultiplicationTableTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import dev.karina.java_loops.MultiplicationTable.MultiplicationTable;
 
-public class MultiplicationTableTest {
+
+public class MultiplicationTableTest{
 
     @Test
     @DisplayName("Test the multiplication table of 5")
@@ -24,6 +24,7 @@ public class MultiplicationTableTest {
                 "5 x 7 = 35", "5 x 8 = 40", "5 x 9 = 45",
                 "5 x 10 = 50");
         List<String> result = MultiplicationTable.generateTable(n);
+        assertEquals(expected, result);
 
         assertThat(result, contains(
                 "5 x 1 = 5", "5 x 2 = 10", "5 x 3 = 15",
